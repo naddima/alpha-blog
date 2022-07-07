9bec14d9fcb685352c7a1ee4ff5cfd1c954bd3bf
naddima <naddima88@gmail.com>
2022-07-07 07:43:29 +0000

    add root, pages controller, home page, about page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "5101666809714388ac6fe88f2101edb1.vfs.cloud9.us-east-1.amazonaws.com"
+
 end
