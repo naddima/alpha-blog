3e48e5a41b59533ed1857d4b9bb1e8132ca14330
naddima <naddima88@gmail.com>
2022-07-09 17:31:39 +0000

    add new.html, add validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-    validates :title, presence: true, length: { minimum: 3, maximum: 40 }
-    validates :description, presence: true, length: { minimum: 8, maximum: 200 }
+    validates :title, presence: true, length: { minimum: 6, maximum: 40 }
+    validates :description, presence: true, length: { minimum: 10, maximum: 200 }
 
 end
