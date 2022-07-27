5155aad788d08349f3c958ccf2b463256db8c6d1
naddima <naddima88@gmail.com>
2022-07-27 13:29:20 +0300

    add new users signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-root 'pages#home'
-get 'about', to: 'pages#about'
-resources :articles                                      #, only: [:show, :index, :new, :create, :edit, :update, :destroy]
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#home"
+  get "about", to: "pages#about"
+  resources :articles                                      #
+  get "signup", to: "users#new"
+  resources :users, except: [:new]
 end
