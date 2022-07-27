07f562532c0d9fd2105e88f407c5a4690cb88374
naddima <naddima88@gmail.com>
2022-07-27 19:49:41 +0300

    Add user index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
-
   def show
     @user = User.find(params[:id])
     @articles = @user.articles
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
   end
 
   def create
