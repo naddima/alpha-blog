0cbfb5343815dab7df75c33bd74d9f18ceb5d6d8
naddima <naddima88@gmail.com>
2022-07-29 21:29:46 +0300

    Add different colors for messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -39,6 +39,7 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article.destroy
+    flash[:alert] = "Article was deleted"
     redirect_to articles_path, status: :see_other
   end
 
