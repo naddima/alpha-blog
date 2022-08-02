e3b8671b468aac4e53da49e48815a5a5e37a4301
naddima <naddima88@gmail.com>
2022-08-02 15:17:20 +0300

    Add categories views and admin permission to create new categories

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
+  setup do
+    @admin_user = User.create(username: "Stallone", email: "stallone@ukr.net", password: "password123", admin: true)
+    sign_in_as(@admin_user)
+  end
+
   test "get new category form and create category" do
     get "/categories/new"
     assert_response :success
@@ -11,5 +16,16 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_response :success
     assert_match "Sports", response.body
+  end
+
+  test "get new category form and reject invalid category submission" do
+    get "/categories/new"
+    assert_response :success
+    assert_no_difference "Category.count" do
+      post categories_path, params: { category: { name: " " } }
+    end
+    assert_match "errors", response.body
+    assert_select "div.alert"
+    assert_select "h4.alert-heading"
   end
 end
